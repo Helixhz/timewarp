ad3ab3278c2bc9ee239da3ce6bd60334bfd61d6a
Helixhz <147705738+Helixhz@users.noreply.github.com>
2023-10-12 13:30:18 -0300

    Update Menu Inicial
    Nova interface e botões funcionando

--- Time Warp - Tower Defense Game/Assets/Scripts/UI/Component_Buttons.cs
+++ Time Warp - Tower Defense Game/Assets/Scripts/UI/Component_Buttons.cs
@@ -1,8 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Component_Buttons : MonoBehaviour
 {
+    public float   originalFontSize = 55f;
+    public Color defaultColor, highlightColor;
 
+    [Space]
+    public GameObject[] buttons;   
+
+    // ==============================================
+    // On Click
+
+    public void OpenMenu(GameObject menu)
+    {
+        menu.SetActive(true);
+        gameObject.SetActive(false);
+
+        DisableHighligth();
+    }
+
+    public void SceneLoader(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+     
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // ==============================================
+    // Highligths
+    
+    public void HighlightText(TMP_Text textTMP)
+    {
+        DisableHighligth();
+        
+        textTMP.fontSize = 65f;
+        textTMP.color = highlightColor;
+    }
+
+    public void DisableHighligth()
+    {
+        foreach(GameObject btn in buttons)
+        {
+            TMP_Text textTMP = btn.transform.GetChild(0).GetComponent<TMP_Text>();
+
+            textTMP.fontSize = originalFontSize;
+            textTMP.color = defaultColor;
+        }
+    }
 }
